ff0b8f32d752f668af905bcb27960a963496fc20
rebeckalscarlsson <rebecka.ls.carlsson@gmail.com>
2019-02-18 13:21:35 +0100

    Skapat MVC-projekt och klasser

---
+++ QuizIt/QuizIt/Models/Question.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizIt.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string TrackQuestion { get; set; }
+        public string Answer { get; set; }
+
+        public Track Track { get; set; }
+        public int TrackId { get; set; }
+    }
+}
