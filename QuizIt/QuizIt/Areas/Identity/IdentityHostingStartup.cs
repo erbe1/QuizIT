c0cf5d7b4c9cea17994772bad9c572372ca422f5
erremolerup <erika.molinder@gmail.com>
2019-02-22 10:56:02 +0100

    Scaffoldat alla filer så att det går att komma åt att ändra språk

---
+++ QuizIt/QuizIt/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using QuizIt.Data;
+
+[assembly: HostingStartup(typeof(QuizIt.Areas.Identity.IdentityHostingStartup))]
+namespace QuizIt.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
