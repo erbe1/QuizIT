52a559197d6e06b5e17c14687ef41f0d8def55ac
rebeckalscarlsson <rebecka.ls.carlsson@gmail.com>
2019-02-26 14:26:56 +0100

    Användarens namn skickas med från clienten till servern som skapar ett meddelande med aktuellt klockslag som skrivs ut!

--- QuizIt/QuizIt/Hubs/QuizHub.cs
+++ QuizIt/QuizIt/Hubs/QuizHub.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System;
 using System.Threading.Tasks;
 
 namespace QuizIt.Hubs
 {
     public class QuizHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        public async Task SendMessage(string user)
         {
+            string message = $" tryckte på knappen {DateTime.Now.Hour} : {DateTime.Now.Minute} : {DateTime.Now.Second} : {DateTime.Now.Millisecond}";
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
     }
